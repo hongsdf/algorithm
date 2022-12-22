16ed7fdcd875b988cac859f2d2d395ff74a4c0ed
hongdsf <tyuz32450@gmail.com>
2022-12-23 00:23:18 +0900

    잡다문제

--- src/algorithm/JobDA.java
+++ src/algorithm/JobDA.java
@@ -1,11 +1,14 @@
 package algorithm;
 
+import java.util.Stack;
+
 public class JobDA {
 	public static void main(String[] args) {
 		Solve m = new Solve();
 		int n = 6;
 		int k = 2;
-		int h[] = {1,3,5,4,2,6};
+		int h[] = {1,7,2,3,8,5};// 1,7,2,3,8,5
+		// 1,3,5,4,2,6
 		// return 2
 		
 		
@@ -21,12 +24,91 @@ public class JobDA {
 	}
 }
 class Solve{
+	public int check[];
+	public int k;
+	public int h[];
 	public int solution(int n,int k,int h[]) {
 		int answer = 0;
+		check = new int[n];
 		// 일반 타워, 컨트롤 타워가 존재하고
-		// 처음 컨
+		// 처음 모든 타워는 일반타워이다.
+		// 일반타워는 양옆의 자신보다 높은 타워로 부터 수신을 받아야하며 양쪽에 존재할경우 왼쪽을 우선순위로 받아온다. 
+		// 종료 조건
+		int time = 1; // 시간
+		while(true) {
+			// 모든 타워가 k시간 이내로 송신을 받을 경우 종료
+			if(!checking()) {
+				break;
+			}
+			
+			// 가장 높이가 높은 타워는 반드시 컨트롤 타워가 되야한다.
+			int max = -1;
+			int index = 0; // 최정상 높이 인덱스
+			for(int i = 0; i <check.length; i++) {
+				if(max < h[i]) {
+					max = h[i];
+					index = i;
+				}
+			}
+			
+			check[index] = 99999; // 컨트롤 타워
+			answer++; // 컨트롤 타워 갯수 증가
+			
+			// 1.일반 타워 구현 시작
+			// 1-1. 컨트롤 타워 바로 옆 구현
+			if(index - 1 >= 0 ) {
+				check[index-1] = time;
+			}
+			
+			if(index + 1 < n ) {
+				check[index+1] = time;
+			}
+			// 1-2. 
+			boolean leftCheck = false;
+			boolean rightCheck = false;
+			//왼쪽 진행
+			int left = 0;
+			
+			// 왼쪽 stack구현
+			Stack<Integer> st1 = new Stack<>();
+			//st1.push(index-1);
+			
+			for(int i = index-2; i >=0; i--) {
+				boolean flag = false;
+				while(!st1.isEmpty() && h[st1.peek()] > h[i]) {
+					// time++;
+					// index까지 전부 변경
+						check[st1.pop()] = time;
+						flag = true;
+					}
+					if(flag) {
+						time++;
+					}
+					
+					st1.push(i);
+				}
+				
+					
+			
+			// 남은 타워 들
+			check[st1.pop()] = time;
+			//오른쪽 진행
+			for(int i = index; i >0; i--) {
+				
+			}
+			
+			
+		}
 		
 		
 		return answer;
+	}
+	private boolean checking() {
+		for(int i = 0; i <check.length; i++) {
+			if(check[i] > k) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
---
+++ src/algorithm/JobDA2.java
@@ -0,0 +1,115 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+// 문제 시간복잡도 초과
+// long타입 구현 arr[][] = new long[1000000000][1000000000]; 3개가 존재 메모리 초과
+public class JobDA2 {
+	public static void main(String[] args) {
+		Solve2 m = new Solve2();
+		int n = 4;
+//		int x1[] = {2,3,5,1};
+//		int y1[] = {1,3,3,7};
+//		int x2[] = {4,5,7,3};
+//		int y2[] = {3,5,7,9};
+		
+		
+		int x1[] = {10,20,40,10};
+		int y1[] = {100,40,60,10};
+		int x2[] = {50,50,90,30};
+		int y2[] = {110,60,90,40};
+		
+//		int x1[] = {10,20,40,10};
+//		int y1[] = {10,40,60,100};
+//		int x2[] = {30,50,90,50};
+//		int y2[] = {40,60,90,110};
+		
+		int answer[] = m.solution(n, x1, y1, x2, y2);
+		for(int a :answer) {
+			System.out.print(a+" ");
+		}
+	}
+}
+class Solve2{
+	public int arr[][] = new int[111][111]; // 1000000000
+	public int gameboard[][] = new int[111][111]; // 1000000000
+	public boolean visit[][] = new boolean[111][111]; 
+	public int cnt = 0; // 섬지역 갯수 체크
+	public HashSet<Integer> set = new HashSet<>();
+	
+	public int[] solution(int n, int x1[],int y1[],int x2[],int y2[]) {
+		int answer[] = new int[n];
+		
+		// gameboard 배열을 하나 더 만든다.
+		// 섬 영역별로 세팅한다.
+		for(int i = 0; i <n; i++) {
+			int row1 = 111 - x1[i];
+			int row2 = 111 - x2[i];
+			
+			int col1 = y1[i];
+			int col2 = y2[i];
+			// gameboard 채우기
+			for(int x = row2; x<row1; x++) {
+				for(int y = col1; y<col2; y++) {
+					gameboard[x][y] = i+1;
+				}
+			}
+		}
+		
+		// 구현
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j <arr.length; j++) {
+				if(!visit[i][j] && gameboard[i][j] != 0 ) {
+					List<Integer> list = new ArrayList<>();
+					int num = gameboard[i][j]; // 섬번호
+					cnt = 1;
+					list.add(num);
+					set.add(num);
+					dfs(i,j,num,list); // 연결된섬을 모두 연결 
+					for(int number : list) {
+						answer[number-1] = cnt;
+					}
+				}
+				
+			}
+		}
+		
+		
+		
+		return answer;
+	}
+
+	private void dfs(int x, int y,int num,List<Integer> list) {
+		// 탈출조건 범위를 초과하지 않고 방문했던 지점이면
+		if(visit[x][y]) return;
+		visit[x][y] = true;
+		
+		
+		// 순환하다가 다른 섬이 나온다면
+		if(num != gameboard[x][y]) {
+			if(!set.contains(gameboard[x][y])) {
+				set.add(num);
+				list.add(gameboard[x][y]);
+				num = gameboard[x][y]; // 넘버 교체
+				cnt++;
+				
+			}
+		}
+		
+		
+		if(x-1 >= 0 && gameboard[x-1][y] != 0 ) { // 상
+			dfs(x-1,y,num,list);
+		}
+		if(x+1 < arr.length && gameboard[x+1][y] != 0 ) { // 하
+			dfs(x+1,y,num,list);
+		}
+		if(y-1 >= 0 && gameboard[x][y - 1] != 0  ) { //  좌
+			dfs(x,y-1,num,list);
+		}
+		if(y+1 < arr.length && gameboard[x][y+1] != 0 ) { // 우
+			dfs(x,y+1,num,list);
+		}
+	}
+}
